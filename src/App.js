997b16c8b06f85748dc6605bf6fa5b07f144d633
shahnaz200git config --global user.name nameshahnaz200 <ueshvd@yahoo.com>
2022-10-14 14:33:12 +0200

    myChat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import NavbarChat from "./nav-chat";
+import React from 'react'
+import Chat from "./pages/chat";
+import Contact from "./pages/contact";
+import Profile from "./pages/profile";
+import { Route, Routes } from "react-router-dom"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+ return (
+   <>
+     <NavbarChat />
+     <div>
+       <Routes>
+         <Route path="/chat" element={<Chat />} />
+         <Route path="/profile" element={<Profile />} />
+         <Route path="/contact" element={<Contact />} />
+      </Routes> 
+     </div>
+   </>
+ )
 }
 
-export default App;
+export default App
